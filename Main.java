d5d7d9f02b96ce0f94bd8f7487ca3967f8e315ea
mhmdrazn <razan4944@gmail.com>
2024-11-25 11:24:11 +0700

    initialize code source

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Playing Tic Tac Toe");
+        TTTConsoleNonOO TTT = new TTTConsoleNonOO();
+        
+        // i want to start the TTT game
+        TTT.main(args);
+    }
+}
