5ca9ab47e21133c0330406c07539ba7d561007aa
mhmdrazn <razan4944@gmail.com>
2024-12-04 08:04:46 +0700

    initialize project

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #4
+ * 1 - 5026231012 - Zihni Aryanto Putra Buana
+ * 2 - 5026231085 - Firmansyah Adi Prasetyo
+ * 3 - 5026231174 - Muhamamd Razan Parisya Putra
+ */
+
 package src.tictactoe;
 
 public class Main {
